e32ba10e03ad1fdf472d8e94fe157e0a650143b9
RLAlpha49 <75044176+RLAlpha49@users.noreply.github.com>
2023-10-20 16:42:22 -0500

    commit -Added a function to sort the text file -Added config to extract text more accurately -Added a check for if the "drop" tab was selected

--- main.py
+++ main.py
@@ -4,15 +4,59 @@ import time
 import keyboard
 import os
 import sys
+import re
 import pygetwindow as gw
 from configparser import ConfigParser
 from PIL import ImageGrab
+
+# Config for Tesseract-OCR to extract text more accurately
+custom_config = r'--oem 3 --psm 6 -c tessedit_char_whitelist=ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789- '
 
 def get_RL_window():
     # Find the Rocket League window by its title
     window = gw.getWindowsWithTitle("Rocket League")[0]
     return window
 
+def clean_text(text):
+    # Use a regular expression to keep only alphanumeric characters and spaces
+    cleaned_text = re.sub(r'[^a-zA-Z0-9\s]', '', text)
+    return cleaned_text
+
+def sort_text_file():
+    # Read the existing file and store its contents in a data structure
+    with open(items_file_path, 'r') as file:
+        lines = file.readlines()
+
+    categories = {}  # A dictionary to store categories and their items
+
+    current_category = None
+    for line in lines:
+        line = line.strip()
+        if line.startswith('[') and line.endswith(']'):
+            current_category = line[1:-1]
+            categories[current_category] = []
+        elif line:  # Ignore empty lines
+            categories[current_category].append(line)
+
+    # Sort the items within each category
+    for category, items in categories.items():
+        sorted_items = sorted(items, key=custom_sort)
+        categories[category] = sorted_items
+
+    # Overwrite the file with sorted contents
+    with open(items_file_path, 'w') as file:
+        for category, items in categories.items():
+            file.write(f'[{category}]\n')
+            for item in items:
+                file.write(f'{item}\n')
+            file.write('\n')  # Add an empty line between categories
+ 
+def custom_sort(item):
+    rarities = ["Uncommon", "Rare", "Very Rare", "Import", "Exotic", "Black Market"]
+    rarity = get_rarity(item)
+    rarity_index = rarities.index(rarity) if rarity in rarities else len(rarities)
+    return rarity_index, len(item)
+               
 def pixel_search_in_window(color, left, right, top, bottom, shade=None):
     window = get_RL_window()
     screenshot = ImageGrab.grab(bbox=(window.left, window.top, window.left + 1920, window.top + 1080))
@@ -42,6 +86,9 @@ def load_items():
 def update_items(category, item):
     items = load_items()
 
+    # Clean the item name to remove unwanted characters
+    item = clean_text(item)
+    
     # Ensure the category exists
     if not items.has_section(category):
         items.add_section(category)
@@ -96,70 +143,87 @@ def get_rarity(item):
     return "Unknown"  # If rarity is not recognized
 
 if __name__ == "__main__":
-    user_input = 0
-    while user_input != 1:
-        print("1: Open Drops\n2: Calulate Probabilities")
-        user_input = int(input())
-        if int(user_input) == 2:
-            calculate_probabilities()
-    
-    time.sleep(1)
-    
-    # Register a hotkey (Ctrl + C) to save the results and exit the program
-    keyboard.add_hotkey('ctrl+c', lambda: exit(0))
-
-    # Checks for a drop in menu
-    while not pixel_search_in_window((38, 62, 107), 100, 100, 280, 281, shade=0):
-        window = get_RL_window()
-        print("\nDrop found\n")
-
-        image = ImageGrab.grab(bbox=(window.left + 40, window.top + 335, window.left + 160, window.top + 350))
-        text = pytesseract.image_to_string(image)
+    while True:
+        user_input = 0
+        while user_input != 1:
+            print("1: Open Drops\n2: Calulate Probabilities\n3: Close")
+            user_input = int(input())
+            if int(user_input) == 2:
+                sort_text_file()
+                calculate_probabilities()
+            elif int(user_input) == 3:
+                break
+        if int(user_input) == 3:
+            break
         
-        # Split the extracted text into lines
-        lines = text.split('\n')
-
-        # Loop through the lines and categorize items
-        current_category = None
-        for line in lines:
-            # Remove any leading/trailing whitespace
-            line = line.strip()
-
-            # If the line is not empty, set it as the current category
-            if line:
-                current_category = line
-
-        pyautogui.leftClick(window.left + 100, window.top + 280)
         time.sleep(1)
+        
+        # Register a hotkey (Ctrl + C) to save the results and exit the program
+        keyboard.add_hotkey('ctrl+c', lambda: exit(0))
 
-        while pixel_search_in_window((0, 2, 3), 70, 71, 920, 921, shade=0):
-            pyautogui.leftClick(window.left + 165, window.top + 910)
-            time.sleep(0.1)
-            pyautogui.leftClick(window.left + 850, window.top + 610)
-            time.sleep(8)
+        # Checks for a drop in menu
+        while not pixel_search_in_window((38, 62, 107), 100, 100, 280, 281, shade=0):
+            window = get_RL_window()
+            
+            image = ImageGrab.grab(bbox=(window.left + 30, window.top + 130, window.left + 310, window.top + 190))
+            text = pytesseract.image_to_string(image, config=custom_config, lang='eng')
+            #print(text.lower())
+            
+            if str(text).lower() != "Drop" or "Drops":
+                print("No Drops Left")
+                break
+            
+            print("\nDrop found\n")
 
-            image = ImageGrab.grab(bbox=(window.left + 725, window.top + 200, window.left + 1195, window.top + 240))
+            image = ImageGrab.grab(bbox=(window.left + 40, window.top + 335, window.left + 160, window.top + 350))
             text = pytesseract.image_to_string(image)
-
+            #print((text.lower()).title())
+            
             # Split the extracted text into lines
             lines = text.split('\n')
 
             # Loop through the lines and categorize items
+            current_category = None
             for line in lines:
                 # Remove any leading/trailing whitespace
                 line = line.strip()
 
                 # If the line is not empty, set it as the current category
                 if line:
-                    print(f"Opened {(str(line).lower()).title()}\n")
-                    update_items(current_category, line)
-                    
+                    current_category = line
 
-            pyautogui.leftClick(window.left + 1050, window.top + 990)
+            pyautogui.leftClick(window.left + 100, window.top + 280)
+            time.sleep(1)
+
+            while pixel_search_in_window((0, 2, 3), 70, 71, 920, 921, shade=0):
+                pyautogui.leftClick(window.left + 165, window.top + 910)
+                time.sleep(0.1)
+                pyautogui.leftClick(window.left + 850, window.top + 610)
+                time.sleep(8)
+
+                image = ImageGrab.grab(bbox=(window.left + 725, window.top + 200, window.left + 1195, window.top + 240))
+                text = pytesseract.image_to_string(image)
+
+                # Split the extracted text into lines
+                lines = text.split('\n')
+
+                # Loop through the lines and categorize items
+                for line in lines:
+                    # Remove any leading/trailing whitespace
+                    line = line.strip()
+
+                    # If the line is not empty, set it as the current category
+                    if line:
+                        print(f"Opened {(str(line).lower()).title()}\n")
+                        update_items(current_category, line)
+                        
+
+                pyautogui.leftClick(window.left + 1050, window.top + 990)
+                time.sleep(0.5)
+            print("\nNo more Drop's left checking for more\n")
+            sort_text_file()
+            pyautogui.leftClick(window.left + 130, window.top + 1030)
             time.sleep(0.5)
-        print("\nNo more Drop's left checking for more\n")
-        pyautogui.leftClick(window.left + 130, window.top + 1030)
-        time.sleep(0.5)
 
     # Remove the hotkey when the program is exiting
     keyboard.unhook_all()
